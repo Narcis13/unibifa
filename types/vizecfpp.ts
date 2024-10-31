be1893f43fd3cfb1a15a700ad0b2d8f375597c99
Narcis13 <Narcis75@gmail.com>
2024-10-31 19:54:24 +0200

    f outin

---
+++ types/vizecfpp.ts
@@ -0,0 +1,12 @@
+export interface CreateVizaCFPPDTO {
+    nume: string,
+    nrviza: string,
+    nrvizac:string,
+    dataviza:Date,
+    document:string,
+    explicatii:string,
+    compartiment:string,
+    codang:string,
+    indicator:string,
+    valoare:number
+  }
