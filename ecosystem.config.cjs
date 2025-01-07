d4bfd2f8d4cfe9915005983f7d78c952a333ab2d
Narcis13 <Narcis75@gmail.com>
2025-01-07 17:30:00 +0200

    eco

---
+++ ecosystem.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+      {
+        name: 'ALOP2',
+        port: '3001',
+        exec_mode: 'cluster',
+        instances: 'max',
+        script: './index.mjs'
+      }
+    ]
+  }
